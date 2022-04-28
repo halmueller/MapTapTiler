f7beabbcfb3f224c96db8eb607b789dfe881c81b
Hal Mueller <halmueller@gmail.com>
2022-04-27 19:32:59 -0700

    Add headers path.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MapTapTiler",
-            dependencies: []),
+            dependencies: [],
+            publicHeadersPath: "include"
+        ),
         .testTarget(
             name: "MapTapTilerTests",
             dependencies: ["MapTapTiler"]),
